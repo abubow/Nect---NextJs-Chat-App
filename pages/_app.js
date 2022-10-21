d9215da77b78f12f82279b6165348dca3e4e78bc
smainc <40357673+smainc@users.noreply.github.com>
2022-10-21 20:57:09 +0500

    fixed other problems

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ import { ContextProvider } from '../context'
 export default function App({ Component, pageProps }) {
   return (
     <ContextProvider>
+      <div className="demo-disclaimer">
+        use me as username and password as password
+      </div>
       <Component {...pageProps} />
     </ContextProvider>
   );
